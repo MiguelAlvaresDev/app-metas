dfd1a752b7ce78fa6c2c214fee2688635d13b8bd
miguelalvaresdev <miguelalvaresdev@gmail.com>
2024-09-11 15:28:51 -0300

    fundamentos 001

---
+++ index.js
@@ -0,0 +1,13 @@
+let meta = {
+  value: "Ler um livro por mês",
+  checked: false,
+  log: (info) => {
+    console.log(info);
+  },
+};
+
+meta.log(meta.value);
+
+const criarMeta = () => {};
+
+function criarMeta() {}
